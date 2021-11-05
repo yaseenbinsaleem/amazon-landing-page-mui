5a0a1682f43b473fcdfd1a09446ee5b4cf9ffce0
yaseenbinsaleem <83838667+yaseenbinsaleem@users.noreply.github.com>
2021-11-05 05:48:01 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import Homepage from './components/Homepage/Homepage';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <Switch>
+          <Route path='/login'>
+            <Header />
+            <Homepage />
+          </Route>
+          <Route path='/checkout'>
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path='/'>
+            <Header />
+            <Homepage />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
